3c9a91e7d8ef8dc3ecb6c42148de09ecf72f0c8d
austinkelsay <austinkelsay@yahoo.com>
2024-10-06 14:41:08 -0500

    Update copy and styles of promotional carousel

--- src/components/content/carousels/InteractivePromotionalCarousel.js
+++ src/components/content/carousels/InteractivePromotionalCarousel.js
@@ -12,8 +12,8 @@ const promotions = [
   {
     id: 1,
     category: "PLEBDEVS",
-    title: "Developer education / community platform",
-    description: "PlebDevs is your gateway to mastering Bitcoin, Lightning, and Nostr technologies. Join our community of aspiring developers and start your journey today!",
+    title: "Content and Community platform",
+    description: "One of a kind Nostr / Lightning integrated Content and Community platform.",
     icon: "pi pi-code",
     video: "https://plebdevs-bucket.nyc3.cdn.digitaloceanspaces.com/plebdevs-montage.mp4",
   },
@@ -21,7 +21,7 @@ const promotions = [
     id: 2,
     category: "CONTENT",
     title: "Comprehensive Learning Resources",
-    description: "Access our extensive library of courses, videos, and documents. From structured learning paths to hands-on workshops, we've got everything you need to master Bitcoin, Lightning, and Nostr development.",
+    description: "Access the PlebDevs library of courses, videos, and documents.",
     icon: "pi pi-book",
     image: "https://media.istockphoto.com/id/1224500457/photo/programming-code-abstract-technology-background-of-software-developer-and-computer-script.jpg?s=612x612&w=0&k=20&c=nHMypkMTU1HUUW85Zt0Ff7MDbq17n0eVeXaoM9Knt4Q=",
   },
@@ -29,7 +29,7 @@ const promotions = [
     id: 3,
     category: "COMMUNITY",
     title: "Join Our Community of learners / hackers",
-    description: "Connect with other developers, share your projects, and get support from our community of Bitcoin enthusiasts.",
+    description: "Learn and connect with other developers, share your projects, and level up together.",
     icon: "pi pi-users",
     image: "https://pikwizard.com/pw/medium/50238b1cad4ff412fdafc1325efa1c9f.jpg",
   },
@@ -37,7 +37,7 @@ const promotions = [
     id: 4,
     category: "LIGHTNING / NOSTR",
     title: "Lightning and Nostr integrated platform",
-    description: "This platform is the first of its kind to integrate Lightning Network and Nostr protocols, allowing users to send and receive payments and interact with the Nostr network.",
+    description: "All content is published to and pulled from nostr relays and is fully interoperable with comments, zaps, and other nostr clients. Premium content is available for sale with lightning or by subscribing to plebdevs via a lightning subscription.",
     icon: "pi pi-bolt",
     image: "https://www.financemagnates.com/wp-content/uploads/2016/05/Bicoin-lightning.jpg",
   },
@@ -139,32 +139,32 @@ const InteractivePromotionalCarousel = () => {
           </div>
         ) : (
           <>
-            <div className="absolute inset-0 bg-gradient-to-t from-black via-black/70 to-transparent rounded-lg" />
-            <div className={`absolute bottom-0 left-0 p-6 space-y-2 ${isTabView ? 'pb-16' : ''}`}>
+            <div className="absolute inset-0 from-black via-black/70 to-transparent rounded-lg" />
+            <div className={`bg-gray-800/90 rounded-tr-lg absolute bottom-0 left-0 p-4 space-y-2 ${isTabView ? 'pb-16' : 'max-w-[80%]'}`}>
               <div className="uppercase text-sm font-bold text-[#f8f8ff]">{selectedPromotion.category}</div>
-              <h2 className="text-4xl font-bold leading-tight text-white drop-shadow-lg">
+              <h2 className="font-bold leading-tight text-white drop-shadow-lg">
                 {selectedPromotion.title}
               </h2>
               <p className="text-lg text-white drop-shadow-md">{selectedPromotion.description}</p>
-              <div className="flex flex-row gap-2 mt-4">
+              <div className="flex flex-row gap-2">
                 {
                   (() => {
                     switch (selectedPromotion.category) {
                       case "PLEBDEVS":
                         return (
-                          <>
+                          <div className="flex flex-row gap-4 mt-4">
                             <GenericButton onClick={() => router.push('/about')} severity="success" icon={<i className="pi pi-question-circle pr-2 pb-[2px]" />} label="Learn More" className="py-2 font-semibold" size="small" outlined />
                             <GenericButton onClick={() => router.push('/subscribe')} severity="warning" icon={<i className="pi pi-star pr-2 pb-1" />} label="Subscribe" className="py-2 font-semibold" size="small" outlined />
                             <GenericButton onClick={() => router.push('/content?tag=all')} severity="primary" icon={<i className="pi pi-eye pr-2" />} label="View all content" className="py-2 font-semibold" size="small" outlined />
-                          </>
+                          </div>
                         );
                       case "CONTENT":
                         return (
-                          <>
+                          <div className="flex flex-row gap-4 mt-4">
                             <GenericButton onClick={() => router.push('/content?tag=courses')} icon={<i className="pi pi-book pr-2 pb-1" />} label="Courses" className="py-2 font-semibold" size="small" outlined />
                             <GenericButton onClick={() => router.push('/content?tag=videos')} icon={<i className="pi pi-video pr-2" />} label="Videos" className="py-2 font-semibold" size="small" outlined />
                             <GenericButton onClick={() => router.push('/content?tag=documents')} icon={<i className="pi pi-file pr-2 pb-1" />} label="Documents" className="py-2 font-semibold" size="small" outlined />
-                          </>
+                          </div>
                         );
                       case "COMMUNITY":
                         return (
