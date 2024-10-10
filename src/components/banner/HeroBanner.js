b8bc1938226a3e54fd8066fdd28d2b550e799888
austinkelsay <austinkelsay@yahoo.com>
2024-10-10 17:04:26 -0500

    Use dif bg image

--- src/components/banner/HeroBanner.js
+++ src/components/banner/HeroBanner.js
@@ -47,7 +47,7 @@ const HeroBanner = () => {
     return (
         <div className="h-[450px] relative flex justify-center items-center overflow-hidden">
             <Image
-                src={HeroImage}
+                src={"https://media.istockphoto.com/id/1224500457/photo/programming-code-abstract-technology-background-of-software-developer-and-computer-script.jpg?s=612x612&w=0&k=20&c=nHMypkMTU1HUUW85Zt0Ff7MDbq17n0eVeXaoM9Knt4Q="}
                 alt="Banner"
                 quality={100}
                 fill
