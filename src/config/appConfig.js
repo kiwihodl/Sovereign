bf21e85d3613258d96ec658c2bafbe3e43de3623
austinkelsay <austinkelsay@yahoo.com>
2024-10-12 17:07:50 -0500

    Only enable testing relay

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,15 +1,15 @@
 const appConfig = {
     defaultRelayUrls: [
-      "wss://nos.lol/",
-      "wss://relay.damus.io/",
-      "wss://relay.snort.social/",
-      "wss://relay.nostr.band/",
-      "wss://relay.mutinywallet.com/",
-      "wss://relay.primal.net/",
-      "wss://nostr21.com/",
-      "wss://nostrue.com/",
-      "wss://purplerelay.com/",
-      // "wss://relay.devs.tools/"
+      // "wss://nos.lol/",
+      // "wss://relay.damus.io/",
+      // "wss://relay.snort.social/",
+      // "wss://relay.nostr.band/",
+      // "wss://relay.mutinywallet.com/",
+      // "wss://relay.primal.net/",
+      // "wss://nostr21.com/",
+      // "wss://nostrue.com/",
+      // "wss://purplerelay.com/",
+      "wss://relay.devs.tools/"
     ],
     authorPubkeys: ["8cb60e215678879cda0bef4d5b3fc1a5c5925d2adb5d8c4fa7b7d03b5f2deaea", "676c02247668d5b18479be3d1a80933044256f3fbd03640a8c234684e641b6d6"],
     customLightningAddresses: [
