b4534a23c4f9f7f2b2c871cd68f6a97be29cc4cf
austinkelsay <austinkelsay@yahoo.com>
2024-10-16 20:11:06 -0500

    reset relays

--- src/context/NDKContext.js
+++ src/context/NDKContext.js
@@ -11,8 +11,8 @@ const readOnlyRelays = ["wss://nostr21.com/", "wss://nostr.wine/", "wss://yestr.
 export const NDKProvider = ({ children }) => {
   const [ndk, setNdk] = useState(null);
   // todo: remove this after testing phase
-  const [userRelays, setUserRelays] = useLocalStorage("userRelays", appConfig.defaultRelayUrls);
-  // const [userRelays, setUserRelays] = useState([...readOnlyRelays, "wss://relay.devs.tools"]);
+  // const [userRelays, setUserRelays] = useLocalStorage("userRelays", appConfig.defaultRelayUrls);
+  const [userRelays, setUserRelays] = useState([...readOnlyRelays, "wss://relay.devs.tools"]);
 
   const createNDKInstance = (relays) => {
     const allRelays = [...new Set([...appConfig.defaultRelayUrls, ...relays])];
